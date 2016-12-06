8f51c09ec3f64a95fd44fa187db4803fdb9c6bec
Matt Ellis <matell@microsoft.com>
2016-12-05 16:25:11 -0800

    Bring over bootstrapping logic from builtools
    Bring over the bootstrapping logic from dotnet/buildtools so we can get
    a CLI to use.
    
    The boostrapping logic in dotnet/buildtools still assumes a project.json
    based CLI and so it generates a project.json to restore. The preview 3
    RTW tooling no longer supports project.json so this fails.
    
    Sicne we don't need to bring down any additional tools at this point (we
    should be raw MSBuild), I've just removed the logic that handles
    project.json generation and restoration of the packages listed in
    .toolversions.

---
+++ bootstrap/bootstrap.ps1
@@ -0,0 +1,81 @@
+param
+(
+    [Parameter(Mandatory=$false)][string]$RepositoryRoot = $PSScriptRoot,
+    [Parameter(Mandatory=$false)][string]$ToolsLocalPath = (Join-Path $RepositoryRoot "Tools"),
+    [Parameter(Mandatory=$false)][string]$CliLocalPath = (Join-Path $ToolsLocalPath "dotnetcli"),
+    [Parameter(Mandatory=$false)][string]$SharedFrameworkSymlinkPath = (Join-Path $ToolsLocalPath "dotnetcli\shared\Microsoft.NETCore.App\version"),
+    [Parameter(Mandatory=$false)][string]$SharedFrameworkVersion = "<auto>",
+    [Parameter(Mandatory=$false)][string]$Architecture = "<auto>",
+    [switch]$Force = $false
+)
+
+$rootToolVersions = Join-Path $RepositoryRoot ".toolversions"
+$bootstrapComplete = Join-Path $ToolsLocalPath "bootstrap.complete"
+
+# if the force switch is specified delete the semaphore file if it exists
+if ($Force -and (Test-Path $bootstrapComplete))
+{
+    del $bootstrapComplete
+}
+
+# if the semaphore file exists and is identical to the specified version then exit
+if ((Test-Path $bootstrapComplete) -and !(Compare-Object (Get-Content $rootToolVersions) (Get-Content $bootstrapComplete)))
+{
+    exit 0
+}
+
+$initCliScript = "dotnet-install.ps1"
+$dotnetInstallPath = Join-Path $ToolsLocalPath $initCliScript
+
+# blow away the tools directory so we can start from a known state
+if (Test-Path $ToolsLocalPath)
+{
+    # if the bootstrap.ps1 script was downloaded to the tools directory don't delete it
+    rd -recurse -force $ToolsLocalPath -exclude "bootstrap.ps1"
+}
+else
+{
+    mkdir $ToolsLocalPath | Out-Null
+}
+
+# download CLI boot-strapper script
+Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1" -OutFile $dotnetInstallPath
+
+# load the version of the CLI
+$rootCliVersion = Join-Path $RepositoryRoot ".cliversion"
+$dotNetCliVersion = Get-Content $rootCliVersion
+
+if (-Not (Test-Path $CliLocalPath))
+{
+    mkdir $CliLocalPath | Out-Null
+}
+
+# now execute the script
+Write-Host "$dotnetInstallPath -Version $dotNetCliVersion -InstallDir $CliLocalPath -Architecture ""$Architecture"""
+Invoke-Expression "$dotnetInstallPath -Version $dotNetCliVersion -InstallDir $CliLocalPath -Architecture ""$Architecture"""
+if ($LastExitCode -ne 0)
+{
+    Write-Output "The .NET CLI installation failed with exit code $LastExitCode"
+    exit $LastExitCode
+}
+
+# create a junction to the shared FX version directory. this is
+# so we have a stable path to dotnet.exe regardless of version.
+$runtimesPath = Join-Path $CliLocalPath "shared\Microsoft.NETCore.App"
+if ($SharedFrameworkVersion -eq "<auto>")
+{
+    $SharedFrameworkVersion = Get-ChildItem $runtimesPath -Directory | % { New-Object System.Version($_) } | Sort-Object -Descending | Select-Object -First 1
+}
+$junctionTarget = Join-Path $runtimesPath $SharedFrameworkVersion
+$junctionParent = Split-Path $SharedFrameworkSymlinkPath -Parent
+if (-Not (Test-Path $junctionParent))
+{
+    mkdir $junctionParent | Out-Null
+}
+if (-Not (Test-Path $SharedFrameworkSymlinkPath))
+{
+    cmd.exe /c mklink /j $SharedFrameworkSymlinkPath $junctionTarget | Out-Null
+}
+
+# write semaphore file
+copy $rootToolVersions $bootstrapComplete
