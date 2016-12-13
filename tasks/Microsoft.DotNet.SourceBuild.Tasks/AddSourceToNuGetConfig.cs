7b67a9efe00b42af6739a6a73c27e56ea5d24242
Matt Ellis <matell@microsoft.com>
2016-12-12 17:26:03 -0800

    Add Support for building core-setup

---
+++ tasks/Microsoft.DotNet.SourceBuild.Tasks/AddSourceToNuGetConfig.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    /*
+     * This task adds a source to a well-formed NuGet.Config file. If a source with `SourceName` is already present, then
+     * the path of the source is changed. Otherwise, the source is added as the first source in the list, after any clear
+     * elements (if present).
+     */
+    public class AddSourceToNuGetConfig : Task
+    {
+        [Required]
+        public string NuGetConfigFile { get; set; }
+
+        [Required]
+        public string SourceName { get; set; }
+
+        [Required]
+        public string SourcePath { get; set; }
+
+        public override bool Execute()
+        {
+            XDocument d = XDocument.Load(NuGetConfigFile);
+            XElement packageSourcesElement = d.Root.Descendants().First(e => e.Name == "packageSources");
+            XElement toAdd = new XElement("add", new XAttribute("key", SourceName), new XAttribute("value", SourcePath));
+
+            XElement exisitingSourceBuildElement = packageSourcesElement.Descendants().FirstOrDefault(e => e.Name == "add" && e.Attribute(XName.Get("key")).Value == SourceName);
+            XElement lastClearElement = packageSourcesElement.Descendants().LastOrDefault(e => e.Name == "clear");
+
+            if (exisitingSourceBuildElement != null)
+            {
+                exisitingSourceBuildElement.ReplaceWith(toAdd);
+            }
+            else if (lastClearElement != null)
+            {
+                lastClearElement.AddAfterSelf(toAdd);
+            }
+            else
+            {
+                packageSourcesElement.AddFirst(toAdd);
+            }
+
+            using (FileStream fs = new FileStream(NuGetConfigFile, FileMode.Create, FileAccess.ReadWrite))
+            {
+                d.Save(fs);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ tasks/Microsoft.DotNet.SourceBuild.Tasks/GetHostRid.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class GetHostRid : Task
+    {
+        [Output]
+        public string Rid { get; set; }
+
+        public override bool Execute()
+        {
+            string[] lines = File.ReadAllLines("/etc/os-release");
+            DistroInfo result = new DistroInfo();
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("ID=", StringComparison.Ordinal))
+                {
+                    result.Id = line.Substring(3).Trim('"', '\'');
+                }
+                else if (line.StartsWith("VERSION_ID=", StringComparison.Ordinal))
+                {
+                    result.VersionId = line.Substring(11).Trim('"', '\'');
+                }
+            }
+
+            Rid = $"{(result.Id ?? "").ToLowerInvariant()}.{(result.VersionId ?? "").ToLowerInvariant()}-x64";
+
+            return true;
+        }
+    }
+
+    class DistroInfo
+    {
+        public string Id;
+        public string VersionId;
+    }
+}
