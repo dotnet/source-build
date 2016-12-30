bdd7db0065e4f9074f91697e43bac9aeca588a18
Matt Ellis <matell@microsoft.com>
2016-12-29 16:42:29 -0800

    Disable CLI First Run Experience

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+
 ./bootstrap/bootstrap.sh
 ./Tools/dotnetcli/dotnet restore tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
 ./Tools/dotnetcli/dotnet build tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
