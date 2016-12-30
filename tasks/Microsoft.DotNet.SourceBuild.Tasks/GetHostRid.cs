ac6a67db260995d00ba3b26bb29458a525c3abda
Matt Ellis <matell@microsoft.com>
2016-12-30 11:14:13 -0800

    Use Microsoft.DotNet.PlatformAbstractions to compute RID
    Unfortuantly, using PackageReference to
    Microsoft.DotNet.PlatformAbstractions doesn't work because at runtime
    GetHostRid can't load that libray (likely due to MSBuild not
    understanding where to resolve the assembly from).
    
    It looks like this issue may be fixed in newer versions of MSBuild,
    but we might need to move to publishing the tasks so that
    M.D.PlatformAbstractions is SxS with M.D.S.Tasks. Until we get that
    all figured out, vendor in parts of M.D.PlatformAbstractions.

--- tasks/Microsoft.DotNet.SourceBuild.Tasks/GetHostRid.cs
+++ tasks/Microsoft.DotNet.SourceBuild.Tasks/GetHostRid.cs
@@ -2,9 +2,15 @@
 using System.IO;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using Microsoft.DotNet.PlatformAbstractions;
 
 namespace Microsoft.DotNet.Build.Tasks
 {
+    /*
+     * Used Vendoed code from Microsoft.DotNet.PlatformAbstractions.
+     * Long term we should use that library once it is clear how to
+     * get MSBuild on core to be able to load the dependent assembly.
+     */
     public class GetHostRid : Task
     {
         [Output]
@@ -12,29 +18,8 @@ namespace Microsoft.DotNet.Build.Tasks
 
         public override bool Execute()
         {
-            string[] lines = File.ReadAllLines("/etc/os-release");
-            DistroInfo result = new DistroInfo();
-            foreach (string line in lines)
-            {
-                if (line.StartsWith("ID=", StringComparison.Ordinal))
-                {
-                    result.Id = line.Substring(3).Trim('"', '\'');
-                }
-                else if (line.StartsWith("VERSION_ID=", StringComparison.Ordinal))
-                {
-                    result.VersionId = line.Substring(11).Trim('"', '\'');
-                }
-            }
-
-            Rid = $"{(result.Id ?? "").ToLowerInvariant()}.{(result.VersionId ?? "").ToLowerInvariant()}-x64";
-
+            Rid = RuntimeEnvironment.GetRuntimeIdentifier();
             return true;
         }
-    }
-
-    class DistroInfo
-    {
-        public string Id;
-        public string VersionId;
     }
 }
