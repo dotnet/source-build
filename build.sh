71a873b248266c9468e31a47edeaafd1c548cad5
Matt Ellis <matell@microsoft.com>
2016-12-29 19:32:10 -0800

    Invoke MSBuild.dll directly
    This removes the need for the unset dance at the start of every
    command (the syntax of which would differ between *NIX and
    Windows).

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ IFS=$'\n\t'
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 
+SCRIPT_ROOT="$(cd -P "$( dirname "$0" )" && pwd)"
+SDK_VERSION="$(cat $SCRIPT_ROOT/.cliversion)"
+
 ./bootstrap/bootstrap.sh
 ./Tools/dotnetcli/dotnet restore tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
 ./Tools/dotnetcli/dotnet build tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
-./Tools/dotnetcli/dotnet msbuild build.proj "$@"
+./Tools/dotnetcli/dotnet ./Tools/dotnetcli/sdk/$SDK_VERSION/MSBuild.dll build.proj "$@"
