803dc77a364f85f9bb608c7e3e9b1b02fbf0799e
Matt Ellis <matell@microsoft.com>
2017-01-03 17:49:02 -0800

    Add a simple netci.groovy
    Start with just Ubuntu 14.04 to make sure we have all the intergration
    with Jenkins working.

---
+++ netci.groovy
@@ -0,0 +1,32 @@
+import jobs.generation.Utilities;
+import jobs.generation.InternalUtilities;
+
+def project = GithubProject
+def branch = GithubBranchName
+
+[true, false].each { isPR ->
+    ['Debug', 'Release'].each { configuration ->
+
+        def newJobName = InternalUtilities.getFullJobName(project, configuration, isPR)
+
+        def buildString = """build.sh /p:Configuration=${configuration}"""
+
+        def newJob = job(newJobName) {
+            steps {
+                shell("git submodule init")
+                shell("git submodule update")
+                shell(buildString)
+            }
+        }
+
+        Utilities.setMachineAffinity(newJob, "Ubuntu14.04", "latest-or-auto")
+        InternalUtilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+
+        if (isPR) {
+            Utilities.addGithubPRTriggerForBranch(newJob, branch, "Ubuntu 14.04 ${configuration}")
+        }
+        else {
+            Utilities.addGithubPushTrigger(newJob)
+        }
+    }
+}
