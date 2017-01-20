d5598e69bd9d1a652039e24a281519d6881a85cf
Christopher Costa <chcosta@microsoft.com>
2017-01-20 13:23:17 -0800

    Try escaping quotes around args

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ while [[ $# -gt 0 ]]
       exit 0
       ;;
       *)
-      ADDITIONALARGS="$ADDITIONALARGS $key"
+      ADDITIONALARGS="$ADDITIONALARGS \"$key\""
       shift
     esac
 done
@@ -84,8 +84,8 @@ fi
 
 $CLIPATH/dotnet restore tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
 $CLIPATH/dotnet build tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
-echo "$CLIPATH/dotnet $SDKPATH/MSBuild.dll build.proj $ADDITIONALARGS"
-$CLIPATH/dotnet $SDKPATH/MSBuild.dll $SCRIPT_ROOT/build.proj"$ADDITIONALARGS"
+echo "$CLIPATH/dotnet $SDKPATH/MSBuild.dll $SCRIPT_ROOT/build.proj$ADDITIONALARGS"
+$CLIPATH/dotnet $SDKPATH/MSBuild.dll \"$SCRIPT_ROOT/build.proj\"$ADDITIONALARGS
 
 if [[ ! "$CLIPAYLOAD" == "" ]]; then
   echo "Patch CLI with built binaries"
