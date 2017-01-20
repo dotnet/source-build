e9209905a64c9888963fa540966b2caf5a6e3311
Christopher Costa <chcosta@microsoft.com>
2017-01-20 20:56:23 +0000

    Reset branch

--- build.sh
+++ build.sh
@@ -1,14 +1,91 @@
 #!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
 
+usage()
+{
+    echo "Build .NET Core product"
+    echo "build.sh [options]"
+    echo ""
+    echo "Options"
+    echo " -s|--netcore_sdk_path [.NET Core SDK Path]"
+    echo " -h|--help"
+}
+
+CLIPAYLOAD=""
+SDKVERSION=""
+while [[ $# -gt 0 ]]
+  do
+    key="$1"
+
+    case $key in
+      -s|--netcore_sdk_path)
+      CLIPAYLOAD="$2"
+      shift 2
+      ;;
+      -h|--help)
+      usage
+      exit 0
+      ;;
+      *)
+      echo "Unknown argument specified '$key'."
+      usage
+      exit 1
+    esac
+done
+
+set -euo pipefail
+
+CLIPATH="./Tools/dotnetcli"
+SCRIPT_ROOT="$(cd -P "$( dirname "$0" )" && pwd)"
+
+
+if [[ ! "$CLIPAYLOAD" == "" ]] && [[ ! -d "$CLIPAYLOAD" ]]; then
+  echo "--clipayload option specified, but cannot find directory '$CLIPAYLOAD'"
+  exit 1
+fi
+if [[ ! "$CLIPAYLOAD" == "" ]]; then
+  echo "Using specified payload from $CLIPAYLOAD"
+  CLIPATH="$CLIPAYLOAD"
+
+  # Determine SDK Version from cli payload
+  declare -a SDKVERSIONS=()
+  SDKVERSIONS=($(ls $CLIPAYLOAD/sdk))
+  if [[ "${#SDKVERSIONS[@]}" -gt "1" ]]; then
+    echo "Discovered SDK versions:"
+    echo "  ${SDKVERSIONS[*]}"
+    echo "Multiple sdk versions found.  Only one sdk version is permitted, please remove extra sdks."
+    exit 1
+  fi
+  if [[ "${#SDKVERSIONS[@]}" == "0" ]]; then
+    echo "Unable to determine sdk version.  Did you download and extract a '.NET CORE SDK' from https://github.com/dotnet/cli?"
+    exit 1
+  fi
+  SDKVERSION="${SDKVERSIONS[0]}"
+fi
+
+SDKPATH="$CLIPATH/sdk/$SDKVERSION"
+echo "SDKPATH: $SDKPATH"
+
+if [[ ! -d $SDKPATH ]]; then
+  echo "Error: Unable to find CLI sdk at '$SDKPATH'."
+  exit 1
+fi
+
+IFS=$'\n\t'
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 
-SCRIPT_ROOT="$(cd -P "$( dirname "$0" )" && pwd)"
-SDK_VERSION="$(cat $SCRIPT_ROOT/.cliversion)"
+# bootstrap the CLI if no local payload is specified
+if [[ "$CLIPAYLOAD" == "" ]]; then
+  SDKVERSION="$(cat $SCRIPT_ROOT/.cliversion)"
+  ./bootstrap/bootstrap.sh
+fi
+$CLIPATH/dotnet restore tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
+$CLIPATH/dotnet build tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
+echo "$CLIPATH/dotnet $SDKPATH/MSBuild.dll build.proj $@"
+$CLIPATH/dotnet $SDKPATH/MSBuild.dll build.proj "$@"
 
-./bootstrap/bootstrap.sh
-./Tools/dotnetcli/dotnet restore tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
-./Tools/dotnetcli/dotnet build tasks/Microsoft.DotNet.SourceBuild.Tasks/Microsoft.DotNet.SourceBuild.Tasks.csproj
-./Tools/dotnetcli/dotnet ./Tools/dotnetcli/sdk/$SDK_VERSION/MSBuild.dll build.proj "$@"
+if [[ ! "$CLIPAYLOAD" == "" ]]; then
+  echo "Patch CLI with built binaries"
+  $SCRIPT_ROOT/buildscripts/patchpayload.sh --verbose --netcore_sdk_path "$CLIPAYLOAD" --coreclr_repo_path $SCRIPT_ROOT/src/coreclr --corefx_repo_path $SCRIPT_ROOT/src/corefx --coresetup_repo_path $SCRIPT_ROOT/src/core-setup
+fi
+exit 0
---
+++ buildscripts/patchpayload.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+set -euo pipefail
+
+preparePatching()
+{
+  echo "Preparing patch..."
+  PATCHPAYLOAD="$CLIPAYLOAD.patch"
+  mkdir $PATCHPAYLOAD
+  cp $CPARGS --recursive --force $CLIPAYLOAD/* $PATCHPAYLOAD
+  
+  PATCHPAYLOADSHAREDROOT="$PATCHPAYLOAD/shared/Microsoft.NETCore.App"
+  SHAREDVERSIONS=($(ls --reverse $PATCHPAYLOADSHAREDROOT))
+  PATCHPAYLOADSHAREDPATH="$PATCHPAYLOADSHAREDROOT/${SHAREDVERSIONS[0]}"
+  
+  PATCHPAYLOADSDKROOT="$PATCHPAYLOAD/sdk"
+  SDKVERSIONS=($(ls --reverse $PATCHPAYLOADSDKROOT))
+  PATCHPAYLOADSDKPATH="$PATCHPAYLOADSDKROOT/${SDKVERSIONS[0]}"
+  
+  PATCHPAYLOADHOSTROOT="$PATCHPAYLOAD/host/fxr"
+  HOSTVERSIONS=($(ls --reverse $PATCHPAYLOADHOSTROOT))
+  PATCHPAYLOADHOSTPATH="$PATCHPAYLOADHOSTROOT/${HOSTVERSIONS[0]}"
+}
+
+patchCoreClr()
+{
+   echo "Patching runtime binaries..."
+   cp $CPARGS $CORECLRBINDIR/*so "$PATCHPAYLOADSHAREDPATH"
+   cp $CPARGS "$CORECLRBINDIR/corerun" "$PATCHPAYLOADSHAREDPATH"
+   cp $CPARGS "$CORECLRBINDIR/crossgen" "$PATCHPAYLOADSHAREDPATH"
+}
+
+patchCoreFx()
+{
+   echo "Patching framework binaries..."
+   cp $CPARGS $COREFXBINDIR/System.* "$PATCHPAYLOADSHAREDPATH"
+}
+
+patchCoreSetup()
+{
+  echo "Patching SDK binaries..."
+  cp $CPARGS "$CORESETUPBINDIR/dotnet" "$PATCHPAYLOAD"
+  cp $CPARGS "$CORESETUPBINDIR/dotnet" "$PATCHPAYLOADSHAREDPATH"
+  cp $CPARGS "$CORESETUPBINDIR/libhostpolicy.so" "$PATCHPAYLOADSHAREDPATH"
+  cp $CPARGS "$CORESETUPBINDIR/libhostfxr.so" "$PATCHPAYLOADSHAREDPATH"
+  cp $CPARGS "$CORESETUPBINDIR/libhostpolicy.so" "$PATCHPAYLOADSDKPATH"
+  cp $CPARGS "$CORESETUPBINDIR/libhostfxr.so" "$PATCHPAYLOADSDKPATH"
+  cp $CPARGS "$CORESETUPBINDIR/libhostfxr.so" "$PATCHPAYLOADHOSTPATH"
+}
+
+finalizePatching()
+{
+  echo "Finalizing patch..."
+  cp --recursive --force $PATCHPAYLOAD/* $CLIPAYLOAD
+  rm -dfr $PATCHPAYLOAD
+}
+
+CLIPAYLOAD=""
+CORECLRBINDIR=""
+COREFXBINDIR=""
+CORESETUPBINDIR=""
+CPARGS="--force"
+
+while [[ $# -gt 0 ]]
+  do
+    key="$1"
+
+    case $key in
+      -p|--netcore_sdk_path)
+      CLIPAYLOAD="$2"
+      shift 2
+      ;;
+      -c|--coreclr_repo_path)
+      CORECLRBINDIR="$2/bin/Product/Linux.x64.Release"
+      shift 2
+      ;;
+      -f|--corefx_repo_path)
+      COREFXBINDIR="$2/bin/Linux.x64.Release/native"
+      shift 2
+      ;;
+      -s|--coresetup_repo_path)
+      CORESETUPOSPATH=($(ls $2/artifacts))
+      CORESETUPBINDIR="$2/artifacts/${CORESETUPOSPATH[0]}/corehost"
+      shift 2
+      ;;
+      -v|--verbose)
+      CPARGS="$CPARGS --verbose"
+      shift 1
+      ;;
+      *)
+      echo "Unknown argument specified '$key'."
+      exit 1
+    esac
+done
+
+missingdir="0"
+if [[ ! -d $CLIPAYLOAD ]]; then
+  echo "Error: Unable to find CLI directory to patch, specify CLI directory with the '--netcore_sdk_path' option."
+  missingdir="1"
+fi
+if [[ ! -d $CORECLRBINDIR ]]; then
+  echo "Error: Unable to find CoreClr bin directory, specify CoreClr repo path with the '--coreclr_repo_path' option."
+  missingdir="1"
+fi
+if [[ ! -d $COREFXBINDIR ]]; then
+  echo "Error: Unable to find CoreFx bin directory, specify CoreFx repo path with the '--corefx_repo_path' option."
+  missingdir="1"
+fi
+if [[ ! -d $CORESETUPBINDIR ]]; then
+  echo "Error: Unable to find CoreSetup bin directory, specify CoreSetup repo path with the '--coresetup_repo_path' option."
+  missingdir="1"
+fi
+if [[ "$missingdir" == "1" ]]; then
+  exit 1
+fi
+
+preparePatching
+patchCoreClr
+patchCoreFx
+patchCoreSetup
+finalizePatching
+
+exit 0
