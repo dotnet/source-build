f24727d516da5830da376e993aa0306db06193b1
karajas <karajas@microsoft.com>
2017-04-01 00:49:41 +0000

    Address feedback

--- overridebuildscripts/init-tools.sh
+++ overridebuildscripts/init-tools.sh
@@ -3,28 +3,27 @@
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
 __init_tools_log=$__scriptpath/init-tools.log
 __PACKAGES_DIR=$__scriptpath/packages
-if [ -n $PACKAGES_DIR ];then __PACKAGES_DIR=$PACKAGES_DIR; fi
+if [ ! -z $PACKAGES_DIR ];then __PACKAGES_DIR=$PACKAGES_DIR; fi
 __TOOLRUNTIME_DIR=$__scriptpath/Tools
 __TOOLOVERRIDE_DIR=$__scriptpath/Tools-Override
 __DOTNET_PATH=$__TOOLRUNTIME_DIR/dotnetcli
-if [ ! -n $DOTNET_PATH ];then __DOTNET_PATH=$DOTNET_PATH; fi
+if [ ! -z $DOTNET_DIR ];then __DOTNET_PATH=$DOTNET_DIR; fi
 __DOTNET_CMD=$__DOTNET_PATH/dotnet
 __BUILD_TOOLS_PACKAGE_VERSION=$(cat $__scriptpath/BuildToolsVersion.txt)
-__BUILD_TOOLS_PATH=$__PACKAGES_DIR/Microsoft.DotNet.BuildTools/$__BUILD_TOOLS_PACKAGE_VERSION/lib
 __INIT_TOOLS_DONE_MARKER=$__TOOLRUNTIME_DIR/done
 
 if [ ! -e $__INIT_TOOLS_DONE_MARKER ]; then
     if [ -e $__TOOLRUNTIME_DIR ]; then rm -rf -- $__TOOLRUNTIME_DIR; fi
     echo "Running: $__scriptpath/init-tools.sh" > $__init_tools_log
-
+    
     if [ ! -e $__DOTNET_PATH ]; then
-        mkdir -p "$__DOTNET_PATH"
+        mkdir -p "$__DOTNET_PATH" 
         if [ -n "$DOTNET_TOOLSET_DIR" ]; then
             echo "Copying $DOTNET_TOOLSET_DIR to $__DOTNET_PATH" >> $__init_tools_log
             cp -r $DOTNET_TOOLSET_DIR/* $__DOTNET_PATH
         fi
     fi
-   
+    
     if [ -n "$BUILD_TOOLS_TOOLSET_DIR" ] && [ -d "$BUILD_TOOLS_TOOLSET_DIR/$__BUILD_TOOLS_PACKAGE_VERSION" ]; then
         echo "Initializing BuildTools..."
         echo "Running: $BUILD_TOOLS_TOOLSET_DIR/$__BUILD_TOOLS_PACKAGE_VERSION/lib/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR" >> $__init_tools_log
---
+++ overridebuildscripts/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+working_tree_root="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set Platform=
+toolRuntime=$TOOLRUNTIME_DIR
+dotnet=$toolRuntime/dotnetcli/dotnet
+echo "Running: $dotnet $toolRuntime / run.exe $working_tree_root / config.json $*"
+$dotnet $toolRuntime/run.exe $working_tree_root/config.json $*
+if [ $? -ne 0 ];then
+exit 1
+fi
+exit 0
