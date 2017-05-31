c6bafca49c45763249a57570eae9f5827a83575f
karajas <karajas@microsoft.com>
2017-05-31 09:36:47 -0700

    Update submodules and add targets

--- armdeps.sh
+++ armdeps.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 apt-get install qemu qemu-user-static binfmt-support debootstrap libxml2-utils docker binutils-arm-linux-gnueabihf
 ./cross/build-rootfs.sh armel tizen
