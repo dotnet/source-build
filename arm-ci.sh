f0b0471dceb4ce4f6cd3266b54711a480d37c9e8
karajas <karajas@microsoft.com>
2017-07-05 13:53:25 -0700

    Add arm ci script

---
+++ arm-ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+__currentWorkingDirectory=`pwd`
+__buildArch=$1
+shift;
+
+# Check build configuration and choose Docker image
+__dockerEnvironmentVariables=""
+if [ "$__buildArch" == "arm" ]; then
+    __dockerImage=" microsoft/dotnet-buildtools-prereqs:ubuntu-14.04-cross-0cd4667-20172211042239"
+    __dockerEnvironmentVariables+=" -e ROOTFS_DIR=/crossrootfs/arm"
+elif [ "$__buildArch" == "armel" ]; then
+    __dockerImage=" hqueue/dotnetcore:ubuntu1404_cross_prereqs_v4-tizen_rootfs"
+    __dockerEnvironmentVariables+=" -e ROOTFS_DIR=/crossrootfs/armel.tizen.build"
+fi
+__dockerCmd="sudo docker run ${__dockerEnvironmentVariables} --privileged -i --rm -v $__currentWorkingDirectory:/opt/code -w /opt/code $__dockerImage"
+$__dockerCmd "$@"
