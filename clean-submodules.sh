eadfd06ba918be73b13ce46b05e93fcac9824881
Matt Ellis <matell@microsoft.com>
2017-07-31 11:55:29 -0700

    Checkpoint

--- clean-submodules.sh
+++ clean-submodules.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
+SCRIPT_ROOT="$(cd -P "$( dirname "$0" )" && pwd)"
 
 git submodule foreach --recursive git clean -dxf
 git submodule foreach --recursive git reset --hard
 
+git clean -dxf "$SCRIPT_ROOT/src/javascriptservices/"
+git clean -dxf "$SCRIPT_ROOT/src/netcorecli-fsc/"
