59dc793249fa7fb5e568ab2f556cce5b530179f6
Davis Goodin <dagood@microsoft.com>
2017-10-16 15:27:15 -0500

    Fix init-tools.sh to work with -euo pipefail

--- build.sh
+++ build.sh
@@ -15,11 +15,7 @@ export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_MULTILEVEL_LOOKUP=0
 export NUGET_PACKAGES="$SCRIPT_ROOT/packages/"
 
-(
-    # init-tools.sh isn't compatible with these flags. Unset them in this subshell.
-    set +euo pipefail
-    . "$SCRIPT_ROOT/init-tools.sh"
-)
+. "$SCRIPT_ROOT/init-tools.sh"
 
 CLIPATH="$SCRIPT_ROOT/Tools/dotnetcli"
 SDKPATH="$CLIPATH/sdk/$SDK_VERSION"
