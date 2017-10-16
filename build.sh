ef41642820528ed1b003fd09715eaf2c8c36cc17
Davis Goodin <dagood@microsoft.com>
2017-10-16 13:21:13 -0500

    Unset eu, pipefail: init-tools.sh not compatible

--- build.sh
+++ build.sh
@@ -15,7 +15,11 @@ export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_MULTILEVEL_LOOKUP=0
 export NUGET_PACKAGES="$SCRIPT_ROOT/packages/"
 
-. "$SCRIPT_ROOT/init-tools.sh"
+(
+    # init-tools.sh isn't compatible with these flags. Unset them in this subshell.
+    set +euo pipefail
+    . "$SCRIPT_ROOT/init-tools.sh"
+)
 
 CLIPATH="$SCRIPT_ROOT/Tools/dotnetcli"
 SDKPATH="$CLIPATH/sdk/$SDK_VERSION"
