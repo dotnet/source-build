7917cb492e862904e8f740fbd8fa3e49b2a8119a
Eric Erhardt <eric.erhardt@microsoft.com>
2018-05-29 18:17:47 -0500

    Update dotnet-dev-certs version
    This version needs to be compatible with the current runtime being produced.

--- smoke-test.sh
+++ smoke-test.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 SCRIPT_ROOT="$(cd -P "$( dirname "$0" )" && pwd)"
 TARBALL_PREFIX=dotnet-sdk-
 VERSION_PREFIX=2.1
-DEV_CERTS_VERSION_DEFAULT=2.1.0
+# See https://github.com/dotnet/source-build/issues/579, this version
+# needs to be compatible with the runtime produced from source-build
+DEV_CERTS_VERSION_DEFAULT=2.1.0-rtm-30762
 
 projectOutput=false
 keepProjects=false
