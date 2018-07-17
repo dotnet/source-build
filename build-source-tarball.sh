80a78610abb10e2522a786ea55a9268c9e13db22
Davis Goodin <dagood@microsoft.com>
2018-07-17 15:36:02 -0500

    Link workaround tracking issue

--- build-source-tarball.sh
+++ build-source-tarball.sh
@@ -108,7 +108,7 @@ if [ -z "${SOURCE_BUILD_SKIP_PREBUILT_REPORT:-}" ]; then
     )
 fi
 
-echo 'WORKAROUND: Overwriting the source-built roslyn-tools MSBuild files with prebuilt so that roslyn-tools can successfully build in the tarball...'
+echo 'WORKAROUND: Overwriting the source-built roslyn-tools MSBuild files with prebuilt so that roslyn-tools can successfully build in the tarball... (https://github.com/dotnet/source-build/issues/654)'
 
 ROSLYN_TOOLS_PACKAGE='RoslynTools.RepoToolset'
 JSON_LINE=$(grep "$ROSLYN_TOOLS_PACKAGE" "$SCRIPT_ROOT/src/roslyn-tools/global.json")
