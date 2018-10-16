367425e80d3cf87b1c805672aef756481b2bf03e
Davis Goodin <dagood@microsoft.com>
2018-10-16 14:11:48 -0500

    Copy dotnet env vars from build scripts to clean
    Avoid doing first-time initialization during clean script execution.

--- clean.cmd
+++ clean.cmd
@@ -9,6 +9,9 @@ if [%1] == [-a] (
 )
 
 if EXIST "%~dp0Tools\dotnetcli\dotnet.exe" (
+  set DOTNET_CLI_TELEMETRY_OPTOUT=1
+  set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+  set DOTNET_MULTILEVEL_LOOKUP=0
   "%~dp0Tools\dotnetcli\dotnet.exe" msbuild "%~dp0build.proj" -t:Clean
   exit /b %ERRORLEVEL%
 )
