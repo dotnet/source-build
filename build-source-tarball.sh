d8ba435a973a230d9dc3eba049bb7e38bb7f8072
Davis Goodin <dagood@users.noreply.github.com>
2018-10-22 14:10:44 -0500

    Update based on PR comment.
    Co-Authored-By: dseefeld <Dan.Seefeldt@microsoft.com>

--- build-source-tarball.sh
+++ build-source-tarball.sh
@@ -88,7 +88,7 @@ git submodule foreach --quiet --recursive '
     fi'
 
 echo 'Removing binaries from tarball src...'
-find $TARBALL_ROOT/src \( -type f -iname *.dll -o -iname *.exe \) -exec rm {} \;
+find $TARBALL_ROOT/src \( -type f \( -iname *.dll -o -iname *.exe \) \) -exec rm {} \;
 
 echo 'Copying scripts and tools to tarball...'
 
