4fc7d99c1bede20dea716d0e162fac986363438a
Chris Rummel <crummel@microsoft.com>
2019-03-25 18:04:55 -0500

    Add submodule checks.

--- tools-local/tasks/Microsoft.DotNet.SourceBuild.Tasks/CloneRepos.cs
+++ tools-local/tasks/Microsoft.DotNet.SourceBuild.Tasks/CloneRepos.cs
@@ -22,22 +22,31 @@ namespace Microsoft.DotNet.SourceBuild.Tasks
         [Required]
         public string SpecFile { get; set; }
 
+        private IEnumerable<RepoSpec> RepoSpecs { get; set; }
+        private bool ResetAll { get; set; }
+        private bool CheckoutAll { get; set; }
+
         public override bool Execute()
         {
             EnsureGit();
             Directory.CreateDirectory(SourceRoot);
             Directory.CreateDirectory(GitRoot);
+            RepoSpecs = ReadSpecFile(SpecFile);
 
-            Parallel.ForEach(ReadSpecFile(SpecFile), d =>
+            Parallel.ForEach(RepoSpecs, d =>
             {
-                var force = false;
                 if (Directory.Exists(Path.Combine(GitRoot, d.Name + ".git")))
                 {
                     if (!Directory.Exists(Path.Combine(SourceRoot, d.Name)))
                     {
                         Directory.CreateDirectory(Path.Combine(SourceRoot, d.Name));
                         File.WriteAllText(Path.Combine(SourceRoot, d.Name, ".git"), $"gitdir: {Path.Combine(GitRoot, d.Name + ".git")}");
-                        force = true;
+
+                        var checkoutSuccess = CheckoutRepo(Path.Combine(SourceRoot, d.Name), d.Hash, true).Result;
+                        if (!checkoutSuccess)
+                        {
+                            Log.LogError($"Checking out {d.Hash} in repo {Path.Combine(SourceRoot, d.Name)} failed.");
+                        }
                     }
                     Log.LogMessage(MessageImportance.High, $"About to fetch {d.Url} to {Path.Combine(GitRoot, d.Name)}");
                     var fetchSuccess = FetchRepo(d.Url, Path.Combine(SourceRoot, d.Name)).Result;
@@ -56,16 +65,197 @@ namespace Microsoft.DotNet.SourceBuild.Tasks
                         Log.LogError($"Cloning {d.Url} into {Path.Combine(SourceRoot, d.Name)} failed.");
                     }
                     Log.LogMessage(MessageImportance.High, $"Done cloning {d.Url} to {Path.Combine(SourceRoot, d.Name)}");
+
+                    var checkoutSuccess = CheckoutRepo(Path.Combine(SourceRoot, d.Name), d.Hash, false).Result;
+                    if (!checkoutSuccess)
+                    {
+                        Log.LogError($"Checking out {d.Hash} in repo {Path.Combine(SourceRoot, d.Name)} failed.");
+                    }
                 }
 
-                var checkoutSuccess = CheckoutRepo(Path.Combine(SourceRoot, d.Name), d.Hash, force).Result;
-                if (!checkoutSuccess)
-                {
-                    Log.LogError($"Checking out {d.Hash} in repo {Path.Combine(SourceRoot, d.Name)} failed.");
-                }
+                CheckForDirtyRepo(SourceRoot, d.Name).Wait();
             });
 
             return !Log.HasLoggedErrors;
+        }
+
+        private async System.Threading.Tasks.Task CheckForDirtyRepo(string sourceRoot, string name)
+        {
+            var masterSetting = Environment.GetEnvironmentVariable("SOURCE_BUILD_SKIP_SUBMODULE_CHECK");
+            if (masterSetting == "1" || masterSetting == "true" || masterSetting == "yes")
+            {
+                return;
+            }
+
+            await CheckForModificationsInRepo(sourceRoot, name);
+            await CheckForMismatchedRepo(sourceRoot, name);
+        }
+
+        private async System.Threading.Tasks.Task CheckForMismatchedRepo(string sourceRoot, string name)
+        {
+            var path = Path.Combine(sourceRoot, name);
+            var currentHash = (await RunExternalGitCommand("rev-parse --verify HEAD")).StdOut;
+            var spec = RepoSpecs.Single(r => r.Name == name);
+            if (spec.Hash != currentHash)
+            {
+                if (CheckoutAll)
+                {
+                    await CheckoutRepo(path, spec.Hash, true);
+                }
+                var setting = Environment.GetEnvironmentVariable("SOURCE_BUILD_MISMATCHED_SUBMODULE_BEHAVIOR");
+                var mergeBase = "missing commit";
+                // check if the repo has the commit we're looking for
+                if ((await RunExternalGitCommand($"cat-file -e {spec.Hash}^{{commit}}", path)).Success)
+                {
+                    mergeBase = (await RunExternalGitCommand($"git merge-base HEAD {spec.Hash}", path)).StdOut;
+                    Log.LogMessage(MessageImportance.High, $"got merge base ({mergeBase})");
+                }
+                if (string.IsNullOrWhiteSpace(setting) || setting == "prompt" || setting == "warn")
+                {
+                    if (mergeBase != spec.Hash)
+                    {
+                        // diverged
+                        Log.LogWarning($"submodule {name}, currently at {currentHash}, has diverged from the expected hash {spec.Hash}.  If you are changing this submodule's branch or moving it backward, this is expected.");
+                    }
+                    else
+                    {
+                        // ahead
+                        Log.LogWarning($"submodule {name}, currently at {currentHash}, is ahead of the expected hash {spec.Hash}.  If you are updating this submodule, this is expected.");
+                    }
+                }
+                if (string.IsNullOrWhiteSpace(setting))
+                {
+                    DoPromptForCheckout(sourceRoot, name, spec.Hash, true);
+                }
+                else
+                {
+                    if (setting == "prompt")
+                    {
+                        DoPromptForCheckout(sourceRoot, name, spec.Hash, false);
+                    }
+                    else if (setting == "warn" || setting == "ignore")
+                    {
+                        // warn handled above, nothing to do for ignore
+                    }
+                    else if (setting == "reset")
+                    {
+                        await CheckoutRepo(path, spec.Hash, true);
+                    }
+                    else
+                    {
+                        Log.LogWarning($"value {setting} for environment variable SOURCE_BUILD_MISMATCHED_SUBMODULE_BEHAVIOR is not valid.");
+                        DoPromptForCheckout(sourceRoot, name, spec.Hash, true);
+                    }
+                }
+            }
+        }
+
+        private async System.Threading.Tasks.Task CheckForModificationsInRepo(string sourceRoot, string name)
+        {
+            var path = Path.Combine(sourceRoot, name);
+
+            // staged changes and unstaged modifications
+            var dirty = !(await RunExternalGitCommand($"git diff-index --quiet HEAD --", path)).Success;
+            // untracked list
+            var untracked = (await RunExternalGitCommand($"git ls-files --others --exclude-standard", path)).StdOut;
+
+            if (dirty || !string.IsNullOrWhiteSpace(untracked))
+            {
+                if (ResetAll)
+                {
+                    await CleanAndResetRepo(path);
+                }
+                else
+                {
+                    var setting = Environment.GetEnvironmentVariable("SOURCE_BUILD_DIRTY_SUBMODULE_BEHAVIOR");
+                    if (string.IsNullOrWhiteSpace(setting))
+                    {
+                        Log.LogWarning($"Modifications found in repo {name}");
+                        DoPromptForCleanAndReset(sourceRoot, name, true);
+                    }
+                    else
+                    {
+                        if (setting == "prompt")
+                        {
+                            DoPromptForCleanAndReset(sourceRoot, name, false);
+                        }
+                        else if (setting == "warn")
+                        {
+                            Log.LogWarning($"Modifications found in repo {name}");
+                        }
+                        else if (setting == "reset")
+                        {
+                            await CleanAndResetRepo(path);
+                        }
+                        else if (setting == "ignore")
+                        {
+
+                        }
+                        else
+                        {
+                            Log.LogWarning($"value {setting} for environment variable SOURCE_BUILD_DIRTY_SUBMODULE_BEHAVIOR is not valid.");
+                            DoPromptForCleanAndReset(sourceRoot, name, true);
+                        }
+                    }
+                }
+            }
+        }
+
+        private async void DoPromptForCheckout(string sourceRoot, string name, string expectedHash, bool printExtraInfo)
+        {
+            if (printExtraInfo)
+            {
+                Console.WriteLine("You can set the behavior used when submodules differ from the expected hash using the SOURCE_BUILD_MISMATCHED_SUBMODULE_BEHAVIOR environment variable.  Options:");
+                Console.WriteLine("prompt: default behavior.  ask for each mismatched repo.");
+                Console.WriteLine("ignore: do not run this check.  patches may fail to apply unless you set SkipPatches=true.");
+                Console.WriteLine("warn: warn only and continue.");
+                Console.WriteLine("reset: checkout the submodule to the expected hash.  this will lose uncommitted changes by default and without warning.");
+            }
+            var answer = ReadKeyWithRetry($"Would you like me to checkout {name} to the expected hash {expectedHash} (this will lose ALL uncommitted changes)? [N]o / [y]es / [a]ll / [q]uit >", new[] { 'n', 'y', 'a', 'q' });
+            if (answer == 'y')
+            {
+                await CheckoutRepo(Path.Combine(sourceRoot, name), expectedHash, true);
+            }
+            else if (answer == 'a')
+            {
+                CheckoutAll = true;
+                await CheckoutRepo(Path.Combine(sourceRoot, name), expectedHash, true);
+            }
+            else if (answer == 'q')
+            {
+                throw new OperationCanceledException($"User canceled build after checking repo {name} for mismatch.");
+            }
+        }
+
+        private async void DoPromptForCleanAndReset(string sourceRoot, string name, bool printExtraInfo)
+        {
+            if (printExtraInfo)
+            {
+                Console.WriteLine("You can set the behavior used when modifications are found using the SOURCE_BUILD_DIRTY_SUBMODULE_BEHAVIOR environment variable.  Options:");
+                Console.WriteLine("prompt: default behavior.  ask for each dirty repo.");
+                Console.WriteLine("ignore: do not run this check.  patches may fail to apply unless you set SkipPatches=true.");
+                Console.WriteLine("warn: warn only and continue.");
+                Console.WriteLine("reset: clean and reset the repo.  this will lose uncommitted changes by default and without warning.");
+            }
+            var answer = ReadKeyWithRetry($"Would you like me to clean and reset {name} (this will lose ALL uncommitted changes)? [N]o / [y]es / [a]ll / [q]uit >", new[] { 'n', 'y', 'a', 'q' });
+            if (answer == 'y')
+            {
+                await CleanAndResetRepo(Path.Combine(sourceRoot, name));
+            }
+            else if (answer == 'a')
+            {
+                ResetAll = true;
+                await CleanAndResetRepo(Path.Combine(sourceRoot, name));
+            }
+            else if (answer == 'q')
+            {
+                throw new OperationCanceledException($"User canceled build after checking repo {name} for modifications.");
+            }
+        }
+
+        private char ReadKeyWithRetry(string prompt, char[] allowedAnswers)
+        {
+            throw new NotImplementedException();
         }
 
         private async Task<(bool Success, string StdOut, string StdErr)> RunExternalGitCommand(string args, string workingDir = null)
@@ -119,6 +309,12 @@ namespace Microsoft.DotNet.SourceBuild.Tasks
         private async Task<bool> FetchRepo(string url, string path)
         {
             return (await RunExternalGitCommand($"fetch {url}", path)).Success;
+        }
+
+        private async Task<bool> CleanAndResetRepo(string path)
+        {
+            return (await RunExternalGitCommand($"clean -fxd", path)).Success &&
+                   (await RunExternalGitCommand($"reset --hard HEAD", path)).Success;
         }
 
         private async void EnsureGit()
