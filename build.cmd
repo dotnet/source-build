73f51f49a27e3bbfcf93ae4f2d90cd8049dc53b6
Chris Rummel <crummel@microsoft.com>
2019-05-20 10:58:23 -0500

    Revert "windows debugging"
    This reverts commit c790d642db66d721d345c15cddff02c1457ac8bf.

--- build.cmd
+++ build.cmd
@@ -1,9 +1,4 @@
 @echo off
 powershell -NoProfile -NoLogo -Command "& \"%~dp0build.ps1\" %*; exit $LastExitCode;"
-echo on
-echo "crummel debug output windows directories"
-dir /a /s %~dp0 > %~dp0bin\logs\windowsfiles.log
-tree %~dp0 > %~dp0bin\logs\windowstree.log
-dir /a /s %~dp0
 if %errorlevel% neq 0 exit /b %errorlevel%
 
