278aa2bca6d73a7b5bbca806e37df463e6705373
Nikola Milosavljevic (CLR) <nikolam@microsoft.com>
2019-05-31 12:48:48 -0700

    Added null-check - required after merge from dotnet/source-build repo

--- build.ps1
+++ build.ps1
@@ -58,7 +58,7 @@ if ($test)
 	$captured_args += "/t:RunTests"
 }
 
-if (-Not ($captured_args.Contains("/t:") -or $captured_args.Contains("RootRepo")))
+if (($captured_args -eq $null) -or -Not ($captured_args.Contains("/t:") -or $captured_args.Contains("RootRepo")))
 {
   Exec-Block { & "$CLIPATH\dotnet" "$SDKPATH/MSBuild.dll" "$SCRIPT_ROOT/build.proj" /flp:v=diag /bl /clp:v=m /p:RootRepo=arcade $captured_args } | Out-Host
   taskkill /f /im dotnet.exe
