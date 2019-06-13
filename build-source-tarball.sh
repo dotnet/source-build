2d67e3b93c9139e13c1ffc9b9249e651fcd67cf4
Chris Rummel <crummel@microsoft.com>
2019-06-12 19:51:38 -0500

    Add DotNet3CLIVersion to tarball.

--- build-source-tarball.sh
+++ build-source-tarball.sh
@@ -117,6 +117,7 @@ cp $SCRIPT_ROOT/*.props $TARBALL_ROOT/
 cp $SCRIPT_ROOT/*.targets $TARBALL_ROOT/
 cp $SCRIPT_ROOT/init-tools.msbuild $TARBALL_ROOT/
 cp $SCRIPT_ROOT/DotnetCLIVersion.txt $TARBALL_ROOT/
+cp $SCRIPT_ROOT/Dotnet3CLIVersion.txt $TARBALL_ROOT/
 cp $SCRIPT_ROOT/BuildToolsVersion.txt $TARBALL_ROOT/
 cp $SCRIPT_ROOT/ProdConFeed.txt $TARBALL_ROOT/
 cp $SCRIPT_ROOT/smoke-test* $TARBALL_ROOT/
