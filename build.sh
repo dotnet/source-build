50369dd07d0c14a3d227973122e2f82dbfcd85d0
Chris Rummel <crummel@microsoft.com>
2019-09-05 12:48:31 -0500

    Address code review comments.

--- build.sh
+++ build.sh
@@ -2,17 +2,30 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-SCRIPT_ROOT="$(cd -P "$( dirname "$0" )" && pwd)"
-export SDK_VERSION=$(cat $SCRIPT_ROOT/DotnetCLIVersion.txt)
-arcadeLine=`grep -m 1 'Microsoft\.DotNet\.Arcade\.Sdk' "$SCRIPT_ROOT/global.json"`
-pattern="\"Microsoft\.DotNet\.Arcade\.Sdk\" *: *\"(.*)\""
-if [[ $arcadeLine =~ $pattern ]]; then
+# resolve $SOURCE until the file is no longer a symlink
+while [[ -h $source ]]; do
+  scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
+  source="$(readlink "$source")"
+
+  # if $source was a relative symlink, we need to resolve it relative to the path where the
+  # symlink file was located
+  [[ $source != /* ]] && source="$scriptroot/$source"
+done
+
+arcadeLine=`grep -m 1 'Microsoft\.DotNet\.Arcade\.Sdk' "$scriptroot/global.json"`
+sdkLine=`grep -m 1 'dotnet' "$scriptroot/global.json"`
+arcadePattern="\"Microsoft\.DotNet\.Arcade\.Sdk\" *: *\"(.*)\""
+sdkPattern="\"dotnet\" *: *\"(.*)\""
+if [[ $arcadeLine =~ $arcadePattern ]]; then
   export ARCADE_BOOTSTRAP_VERSION=${BASH_REMATCH[1]}
+fi
+if [[ $sdkLine =~ $sdkPattern ]]; then
+  export SDK_VERSION=${BASH_REMATCH[1]}
 fi
 echo "Found bootstrap SDK $SDK_VERSION, bootstrap Arcade $ARCADE_BOOTSTRAP_VERSION"
 
 if [ -z "${HOME:-}" ]; then
-    export HOME="$SCRIPT_ROOT/.home"
+    export HOME="$scriptroot/.home"
     mkdir "$HOME"
 fi
 
@@ -43,25 +56,16 @@ for arg do
 done
 
 if [ "$alternateTarget" == "false" ] && [[ "${SOURCE_BUILD_SKIP_SUBMODULE_CHECK:-default}" == "default" || $SOURCE_BUILD_SKIP_SUBMODULE_CHECK == "0" || $SOURCE_BUILD_SKIP_SUBMODULE_CHECK == "false" ]]; then
-  source "$SCRIPT_ROOT/check-submodules.sh"
+  source "$scriptroot/check-submodules.sh"
 fi
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_MULTILEVEL_LOOKUP=0
-export NUGET_PACKAGES="$SCRIPT_ROOT/packages/"
+export NUGET_PACKAGES="$scriptroot/packages/"
 
 source="${BASH_SOURCE[0]}"
 
-# resolve $SOURCE until the file is no longer a symlink
-while [[ -h $source ]]; do
-  scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-  source="$(readlink "$source")"
-
-  # if $source was a relative symlink, we need to resolve it relative to the path where the
-  # symlink file was located
-  [[ $source != /* ]] && source="$scriptroot/$source"
-done
 
 set -x
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
--- support/tarball/build.sh
+++ support/tarball/build.sh
@@ -3,7 +3,12 @@ set -euo pipefail
 IFS=$'\n\t'
 
 SCRIPT_ROOT="$(cd -P "$( dirname "$0" )" && pwd)"
-CLI_VERSION=$(cat $SCRIPT_ROOT/DotnetCLIVersion.txt)
+sdkLine=`grep -m 1 'dotnet' "$SCRIPT_ROOT/global.json"`
+sdkPattern="\"dotnet\" *: *\"(.*)\""
+if [[ $sdkLine =~ $sdkPattern ]]; then
+  export SDK_VERSION=${BASH_REMATCH[1]}
+fi
+echo "Found bootstrap SDK $SDK_VERSION"
 CLI_ROOT="$SCRIPT_ROOT/.dotnet"
 export SDK_VERSION=$CLI_VERSION
 
@@ -15,12 +20,6 @@ MSBUILD_ARGUMENTS=("/p:OfflineBuild=true" "/flp:v=detailed")
 
 echo "Rebuild reference assemblies"
 $CLI_ROOT/dotnet $CLI_ROOT/sdk/$CLI_VERSION/MSBuild.dll /bl:initBuildReferenceAssemblies.binlog $SCRIPT_ROOT/tools-local/init-build.proj /t:BuildReferenceAssemblies ${MSBUILD_ARGUMENTS[@]} "$@"
-
-echo "Expanding BuildTools dependencies into packages directory..."
-# init-tools tries to copy from its script directory to Tools, which in this case is a copy to
-# itself. This is an error. To avoid the error, use a temp dir that we immediately delete.
-PREBUILT_PACKAGE_SOURCE="$SCRIPT_ROOT/prebuilt/nuget-packages"
-REF_PACKAGE_SOURCE="$SCRIPT_ROOT/reference-packages/packages"
 
 $CLI_ROOT/dotnet $CLI_ROOT/sdk/$CLI_VERSION/MSBuild.dll /bl:initWriteDynamicPropsToStaticPropsFiles.binlog $SCRIPT_ROOT/tools-local/init-build.proj /t:WriteDynamicPropsToStaticPropsFiles /p:GeneratingStaticPropertiesFile=true ${MSBUILD_ARGUMENTS[@]} "$@"
 $CLI_ROOT/dotnet $CLI_ROOT/sdk/$CLI_VERSION/MSBuild.dll /bl:build.binlog $SCRIPT_ROOT/build.proj ${MSBUILD_ARGUMENTS[@]} "$@"
