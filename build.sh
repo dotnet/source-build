1114df37488ae4b1333a74ddeed5e94714c96ac3
adaggarwal <aditya.aggarwal@microsoft.com>
2019-10-16 23:43:14 +0000

    wip

--- build.sh
+++ build.sh
@@ -65,8 +65,6 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_MULTILEVEL_LOOKUP=0
 export NUGET_PACKAGES="$scriptroot/packages/restored/"
-export LD_DEBUG=all
-export LD_DEBUG_OUTPUT="$scriptroot/DEBUG.log"
 
 set -x
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
