470238b47edc6a61c471092ed1d7a5558ded8be3
dseefeld <dseefeld@microsoft.com>
2019-11-15 17:08:09 +0000

    Remove source-built dir from tarball in favor of prev source-built archive - Remove copying of assets to source-built dir in tarball - On initial tarball build, lookup boostrap versions of arcade, sourcelink and DotNetHost from previously source-built archive and use those versions when building arcade, sourcelink and corefx.

--- support/tarball/build.sh
+++ support/tarball/build.sh
@@ -8,7 +8,29 @@ sdkPattern="\"dotnet\" *: *\"(.*)\""
 if [[ $sdkLine =~ $sdkPattern ]]; then
   export SDK_VERSION=${BASH_REMATCH[1]}
 fi
-echo "Found bootstrap SDK $SDK_VERSION"
+
+sourceBuiltArchive=`find ./packages/archive -maxdepth 1 -name 'Private.SourceBuilt.Artifacts*.tar.gz'`
+if [ -f "$sourceBuiltArchive" ]; then
+  tar -xzf "$sourceBuiltArchive" -C /tmp PackageVersions.props
+  arcadeSdkLine=`grep -m 1 'MicrosoftDotNetArcadeSdkVersion' /tmp/PackageVersions.props`
+  versionPattern="<MicrosoftDotNetArcadeSdkVersion>(.*)</MicrosoftDotNetArcadeSdkVersion>"
+  if [[ $arcadeSdkLine =~ $versionPattern ]]; then
+    export ARCADE_BOOTSTRAP_VERSION=${BASH_REMATCH[1]}
+  fi
+
+  sourceLinkLine=`grep -m 1 'MicrosoftSourceLinkCommonVersion' /tmp/PackageVersions.props`
+  versionPattern="<MicrosoftSourceLinkCommonVersion>(.*)</MicrosoftSourceLinkCommonVersion>"
+  if [[ $sourceLinkLine =~ $versionPattern ]]; then
+    export SOURCE_LINK_BOOTSTRAP_VERSION=${BASH_REMATCH[1]}
+  fi
+
+  dotNetHostLine=`grep -m 1 'MicrosoftNETCoreDotNetHostVersion' /tmp/PackageVersions.props`
+  versionPattern="<MicrosoftNETCoreDotNetHostVersion>(.*)</MicrosoftNETCoreDotNetHostVersion>"
+  if [[ $dotNetHostLine =~ $versionPattern ]]; then
+    export DOTNET_HOST_BOOTSTRAP_VERSION=${BASH_REMATCH[1]}
+  fi
+fi
+echo "Found bootstrap SDK $SDK_VERSION, bootstrap Arcade $ARCADE_BOOTSTRAP_VERSION, bootstrap SourceLink $SOURCE_LINK_BOOTSTRAP_VERSION, bootstrap DotNetHost $DOTNET_HOST_BOOTSTRAP_VERSION"
 CLI_ROOT="$SCRIPT_ROOT/.dotnet"
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
