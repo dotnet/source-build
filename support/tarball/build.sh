b6c2aa362d872e7367c8fbf41e387ca9af099056
Dan Seefeldt <dseefeld@microsoft.com>
2020-04-08 11:18:00 -0500

    Add runtime repo (portable and non-portable) and build in master (#1528)
    * Update arcade and standard shas
    
    * Add runtime repo and remove coreclr, corefx and core-setup
    
    * Update standard to build with new arcade version
    
    * Update version of roslyn to work with updated Arcade
    
    * Update patches to enable runtime repo to build
    
    * Update aspnet-extensions commit and get it building
    
    * Add runtime portable build
    
    * Add OverrideTargetRid for Linux
    
    * Retain portable logs when cleaning up after portable build
    
    * Include project.assets.json files in files to be saved from portable build
    
    * Update version of source-build archive
    
    * Remove old corefx, coreclr and core-setup projects and update known-good
    
    * Add patch for ilasm round trip issue
    
    * Add patch for ilasm round trip issue
    
    * Update commit shas for repos in known good for preview 2
    
    * Reconcile patches for core-sdk
    
    * Reconcile patches for linkere
    
    * Reconcile patches for fsharp repo
    
    * Reconcile patches for nuget-client repo
    
    * Reconcile patches for roslyn repo
    
    * Reconcile patches for runtime repo
    
    * Reconcile patches for sdk repo
    
    * Remove dependency flow sources -- update Arcade version
    
    * Update commit sha for standard
    
    * Update Arcade and SDK versions
    
    * Reconcile patches for arcade repo
    
    * Remove ArcadeConfigurationOverride
    
    * Fixup output placement for standard repo
    
    * Remove unneccessary roslyn patch
    
    * Correct invalid merge
    
    * Remove linker reflection heuristics patch per comment from sbomer on https://github.com/dotnet/source-build/issues/777
    
    * Update PackagesOutput for linker
    
    * Add patch to set nopgooptimize in coreclr build
    
    * Update coreclr build arguments
    
    * Only build through coreclr for now
    
    * Only build through runtime
    
    * Add CentOS 8 CI build (#1539)
    
    * Add CentOS 8 CI build
    
    We have had some issues building in RHEL 8, so lets set up a CentOS 8 CI
    system to try and catch those issues earlier.
    
    * Pin to artifacts version with executable coreclr bits
    
    Co-authored-by: Aditya Aggarwal <aditya.aggarwal@microsoft.com>
    Conflicts:
    	eng/Versions.props
    	repos/coreclr.common.props
    
    * Update docker images
    
    * Update version of xliff tasks
    
    * Include 1 addtional arcade patch
    
    * Add patch to exclude Helix tests from source-build in arcade
    
    * Add reference to PlatformAbstractions
    
    * Skip prebuilt check for now
    
    * Update version of previously source-built archive
    
    * Update online prebuilt baseline
    
    * Disable smoke-tests and unit tests until core-sdk build works
    
    * Don't remove placeholder pdb files
    
    * Add back temporary ildasm workaround until AspNetCore builds
    
    * Update baseline with OSX only prebuilts
    
    * Update offline baseline
    
    * Turn off processing of smoke-test-packages
    
    * Update debian & ubuntu images and turn off leak detection
    
    * Update Centos8 docker image to one that includes updated cmake
    
    * Another try at updating the centos8 docker image
    
    * Update to latest version of Private.SourceBuild.ReferencePackages
    
    * Remove detection of and de-assemble/re-assemble of read only ref pkgs
    With the completion of https://github.com/dotnet/source-build/issues/1497,
    the need to detect and remove these read only ref packages
    has been removed.  All packages that we have identified as read-only
    in the past have been included in source-build-reference-packages.
    If any new ones show up, they will show up as prebuilts and can
    be added to the source-build-reference-packages repo.
    
    * Revert "Skip prebuilt check for now"
    
    This reverts commit 5766e7ec6cb245c3348377700ccb3bddda1bf90a.
    
    * Update offline baseline
    
    * Add comments & issue links to disabled items
    
    * Updates based on PR review comments
    
    * Update to document temporary smoke-test removal
    
    * Remove standard project
    Going forward the repo will only be used for servicing.
    
    * Update runtime dependency name
    
    Co-authored-by: Omair Majid <omajid@redhat.com>

--- support/tarball/build.sh
+++ support/tarball/build.sh
@@ -136,8 +136,5 @@ export NUGET_PACKAGES="$SCRIPT_ROOT/packages/restored/"
 
 $CLI_ROOT/dotnet $CLI_ROOT/sdk/$SDK_VERSION/MSBuild.dll /bl:BuildXPlatTasks.binlog $SCRIPT_ROOT/tools-local/init-build.proj /t:BuildXPlatTasks ${MSBUILD_ARGUMENTS[@]} "$@"
 
-echo "Rebuild reference assemblies"
-$CLI_ROOT/dotnet $CLI_ROOT/sdk/$SDK_VERSION/MSBuild.dll /bl:initBuildReferenceAssemblies.binlog $SCRIPT_ROOT/tools-local/init-build.proj /t:BuildReferenceAssemblies ${MSBUILD_ARGUMENTS[@]} "$@"
-
 $CLI_ROOT/dotnet $CLI_ROOT/sdk/$SDK_VERSION/MSBuild.dll /bl:initWriteDynamicPropsToStaticPropsFiles.binlog $SCRIPT_ROOT/tools-local/init-build.proj /t:WriteDynamicPropsToStaticPropsFiles /p:GeneratingStaticPropertiesFile=true ${MSBUILD_ARGUMENTS[@]} "$@"
 $CLI_ROOT/dotnet $CLI_ROOT/sdk/$SDK_VERSION/MSBuild.dll /bl:build.binlog $SCRIPT_ROOT/build.proj ${MSBUILD_ARGUMENTS[@]} "$@"
