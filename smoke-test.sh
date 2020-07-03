26c298f8acd841a8cc9e495c1fdcf6c437541018
Dan Seefeldt <dseefeld@microsoft.com>
2020-07-03 02:39:18 -0500

    Update to Preview4 and include Tier3 repos (#1621)
    * Update to preview4 shas
    
    * Reconcile patches with version updates
    
    * Update arcade version and runtime build commands
    
    * Add buildtools to runtime build
    
    * Update to preview4 shas
    
    * Reconcile patches with version updates
    
    * Add path to ilasm to libraries and installer build
    and capture clrtools from either portable or non-portable builds
    
    * Fix UpdatePackageIndex UsingTask
    
    * Fix subsets for installer
    
    * Add patches for runtime and nuget-client
    
    * Remove unneeded patches for aspnetcore-tooling
    
    * Missed change in rebase
    
    * Update SDK version
    
    * Build through aspnetcore-tooling
    
    * Add netcoreapp5.0 assets to libraries packages
    
    * Don't remove Arcade from restored pkgs cache
    
    * Pickup renamed ILLink.Tasks in runtime
    
    * Re-add arcade patch for tarball build
    
    * Re-enable building source-built-artifacts tarball
    
    * Update online baseline
    
    * Remove pacakge-source-build until sdk is building
    
    * Set PVP Env Variable for all repos
    
    * Set UseStableVersions to false
    
    * Upgrade Roslyn to netcoreapp5.0.
    
    * Update previously-source-built packages to a 3.1+5.0 bootstrap archive.
    
    * Add patch to use MS.Private.CoreFX.NetCoreApp as netcoreapp3.1 in runtime since that's all previously-source-built supports.
    
    * Update patches to build through sdk
    
    * Updates for vstest
    
    * Update fsharp project
    
    * Include Tier 4 repos
    
    * Update prebuilt baseline
    
    * Add and build cosolidated installer project from cli, toolset and core-sdk
    
    * Add patch to update roslyn build path to netcoreapp5.0
    
    * Update smoke-test for 5.0
    
    * Experimental patch for AspNetCore-Tooling.
    
    * Upgrade ref packages and previously-source-built packages.
    
    * Update SBRP version
    
    * Experimental - try live source-built versions for RepoTasks
    
    * Add SDK patch to remove test projects from the solution.
    
    * Add installer patch to exclude test projects from source-build
    
    * Patch F# for source-built versions.
    
    * Add a couple of patches for the installer repo
    
    * Remove supplied VersionSuffix and instead rely on repos to do this
    
    * Update version of vstest.  It wasn't auto updated.
    
    * Add more binlogs to VSTest.
    
    * Upgrade VSTest to netcoreapp5.0.
    
    * Update source-build-artifacts version
    
    * Revert vstest netcoreapp5.0 fix and instead exlude S.Text.Json in sdk
    
    * Correct ordering of projects to put vstest and fsharp before sdk
    
    * Update prebuilt-baselines
    
    * Re-enable smoke-tests
    
    * Re-enable poison report
    
    * Fix LeakDetection task build
    
    * Fix osx build issue
    
    * One more update to fix OSX
    
    * Update to previously source-built
    
    * Updates from PR review
    
    Co-authored-by: Chris Rummel <crummel@microsoft.com>

--- smoke-test.sh
+++ smoke-test.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 SCRIPT_ROOT="$(cd -P "$( dirname "$0" )" && pwd)"
 TARBALL_PREFIX=dotnet-sdk-
-VERSION_PREFIX=3.1
+VERSION_PREFIX=5.0
 # See https://github.com/dotnet/source-build/issues/579, this version
 # needs to be compatible with the runtime produced from source-build
-DEV_CERTS_VERSION_DEFAULT=3.0.0-preview8-28405-07
+DEV_CERTS_VERSION_DEFAULT=5.0.0-preview.3
 __ROOT_REPO=$(cat "$SCRIPT_ROOT/artifacts/obj/rootrepo.txt" | sed 's/\r$//') # remove CR if mounted repo on Windows drive
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
