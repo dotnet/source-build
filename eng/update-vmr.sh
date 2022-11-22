550941fba6e02198efcec78a29294c3de0307865
Logan Bussell <loganbussell@microsoft.com>
2022-11-22 19:05:29 +0000

    Address review comments

--- eng/update-vmr.sh
+++ eng/update-vmr.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-time="$(date +%m%d%y-%H%M)"
-
 print-help ()
 {
    echo "Updates the VMR"
@@ -12,15 +10,15 @@ print-help ()
    echo "options:"
    echo "--releaseChannel, -r  The .NET SDK release channel (e.g. 6.0)"
    echo "--sdkVersion, -v      The .NET SDK version (e.g. 6.0.110)"
-   echo "--upstream, -u        A valid git URL to the upstream repo to base the update branch off of"
+   echo "--upstream, -u        A valid git URL to the upstream repo to update from and push to"
    echo "--tarball, -t         The tarball to update the VMR with"
-   echo "--isDryRun, -d        If set, then don't push results to AzDo"
+   echo "--isDryRun, -d        (Optional) If set, then don't push results to upstream"
    echo "--help, -h            (Optional) print this help message and exit"
    echo
 }
 
-SHORT=r:v:u:t:h
-LONG=releaseChannel:,sdkVersion:,upstream:,tarball:,help
+SHORT=r:v:u:t:dh
+LONG=releaseChannel:,sdkVersion:,upstream:,tarball:,isDryRun,help
 
 OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
 if [ $? != 0 ] ; then echo "Failed to parse options." >&2 ; exit 1 ; fi
@@ -51,7 +49,7 @@ while true ; do
       shift 2
       ;;
     -d | --isDryRun )
-      is_dry_run='true'
+      is_dry_run=true
       shift
       ;;
     -- )
@@ -64,6 +62,11 @@ while true ; do
       ;;
   esac
 done
+
+: ${release_channel:?Missing --releaseChannel}
+: ${sdk_version:?Missing --sdkVersion}
+: ${upstream_url:?Missing --upstream}
+: ${source_tarball:?Missing --tarball}
 
 if [ ! -f "${source_tarball}" ]; then
   echo "##vso[task.logissue type=error]File ${source_tarball} not found on disk. Exiting..."
@@ -81,22 +84,30 @@ rm -rf "${vmr_path}"
 git init "${vmr_path}"
 
 pushd "${vmr_path}"
-    git remote add upstream "${upstream_url}"
-    git fetch upstream "${target_branch}"
+  git remote add upstream "${upstream_url}"
+  git fetch upstream "${target_branch}" --depth=1
 
-    git checkout "${target_branch}"
+  git checkout "${target_branch}"
 
-    new_branch_name="dev/${sdk_version}-${month_year}"
-    git checkout -b "${new_branch_name}"
+  new_branch_name="dev/${sdk_version}-${month_year}"
+  git checkout -b "${new_branch_name}"
 
-    # delete all contents except the .git folder
-    # otherwise we won't catch deleted files in a commit
-    ls | grep -v ".git" | xargs rm -rf
-    tar -xzf "${source_tarball}" -C "${vmr_path}"
+  # delete all contents except the .git folder
+  # otherwise we won't catch deleted files in a commit
+  ls | grep -v ".git" | xargs rm -rf
+  tar -xzf "${source_tarball}" -C "${vmr_path}"
 
-    git add -f .
-    
-    git config user.email "dotnet-maestro[bot]@users.noreply.github.com"
-    git config user.name "dotnet-maestro[bot]"
-    git commit -m "Update to .NET ${sdk_version}"
+  git add -f .
+  
+  git config user.email "dotnet-maestro[bot]@users.noreply.github.com"
+  git config user.name "dotnet-maestro[bot]"
+  git commit -m "Update to .NET ${sdk_version}"
+
+  if [ "$is_dry_run" = true ]; then
+    echo "Doing a dry run, not pushing to upstream. List of changes:"
+    git log --name-status HEAD^..HEAD
+  else
+    echo "Pushing branch to upstream."
+    git push -u upstream "${new_branch_name}"
+  fi
 popd
