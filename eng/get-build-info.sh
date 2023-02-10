23947133bfc02f2bb88270b9a26957ebfa302c88
Premek Vysoky <premek.vysoky@microsoft.com>
2023-02-10 11:10:57 +0100

    Trim spaces in YAML, improve unbound variable use

--- eng/get-build-info.sh
+++ eng/get-build-info.sh
@@ -8,9 +8,7 @@ function get_build_run () {
     local azdo_org="$3"
     local azdo_project="$4"
     local check_build_status="$5"
-    set +u # tag is optional
-    local tag="$6"
-    set -u
+    local tag="${6-}" # Optional
 
     if [[ -n "$tag" ]]; then
         build_runs=$(az pipelines runs list --organization "$azdo_org" --project "$azdo_project" --pipeline-ids "$pipeline_id" --tags "$tag")
@@ -67,9 +65,7 @@ function get_build_info () {
     local pipeline_name="$4"
     local pipeline_variable_name="$5"
     local check_build_status="$6"
-    set +u # tag is optional
-    local tag="$7"
-    set -u
+    local tag="${7-}" # Optional
 
     IFS=' '
     run_info=$(get_build_run "$pipeline_id" "$pipeline_name" "$azdo_org" "$azdo_project" "$check_build_status" "$tag")
