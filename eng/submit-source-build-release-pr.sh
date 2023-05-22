3f0b58854b2834ce267976e6504817db31379ec2
Ali TURAN <alituran@microsoft.com>
2023-05-22 23:13:59 +0200

    test

--- eng/submit-source-build-release-pr.sh
+++ eng/submit-source-build-release-pr.sh
@@ -188,6 +188,7 @@ git add "$global_json_path" "$versions_props_path"
 git config --global user.name "dotnet-sb-bot"
 git config --global user.email "dotnet-sb-bot@microsoft.com"
 git commit -m "update global.json and Versions.props for .NET SDK ${sdk_version}"
+git diff
 
 # push changes to fork
 git push -u origin "${new_branch_name}"
@@ -196,9 +197,9 @@ readarray -d '/' -t fork_repo_split <<< "${fork_repo}"
 fork_owner="${fork_repo_split[0]}"
 
 # create pull request
-gh pr create \
-    --head "${fork_owner}:${new_branch_name}" \
-    --repo "${target_repo}" \
-    --base "${pr_target_branch}" \
-    --title "${title}" \
-    --body "${body}"
+# gh pr create \
+#     --head "${fork_owner}:${new_branch_name}" \
+#     --repo "${target_repo}" \
+#     --base "${pr_target_branch}" \
+#     --title "${title}" \
+#     --body "${body}"
